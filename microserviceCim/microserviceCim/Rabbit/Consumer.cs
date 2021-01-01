9eb2fada762472013850aa7c3306e6adc3f9a5ca
Raul <raulmuve@gmail.com>
2021-01-01 19:58:05 +0100

    [FIX] servicio no arranca si no esta Rabbit conectado

--- microserviceCim/microserviceCim/Rabbit/Consumer.cs
+++ microserviceCim/microserviceCim/Rabbit/Consumer.cs
@@ -12,49 +12,67 @@ namespace microserviceCim.Rabbit
 	{
 		public void ConsumerStart()
 		{
+			Boolean connected = false;
 			String cola = "MicroserveiCims";
 			var factory = new ConnectionFactory() { HostName = "localhost" };
 
 			RepositoryCims repo = new RepositoryCims();
-
-			using (var connection = factory.CreateConnection())
+			while (!connected)
 			{
-				using (var channel = connection.CreateModel())
+				try
 				{
-					channel.QueueDeclare(cola, false, false, false, null);
-
-					var consumer = new EventingBasicConsumer(channel);
-
-					consumer.Received += async (model, ea) =>
+					using (var connection = factory.CreateConnection())
 					{
-						var message = Encoding.UTF8.GetString(ea.Body.ToArray());
-
-						RepositoryCims repo = new RepositoryCims();
-
-						string idCim = message.Split(";")[1];
-
-						Cim cim = await repo.GetCim(idCim);
-
-						switch (message.Split(";")[0])
+						connected = true;
+						if (connected)
 						{
-							case "addRuta":
-								repo.addRuta(cim);
-								break;
-							case "deleteRuta":
-								repo.deleteRuta(cim);
-								break;
-							case "addRefugi":
-								repo.addRefugis(cim);
-								break;
-							case "deleteRefugi":
-								repo.deleteRefugis(cim);
-								break;
+							Console.WriteLine("Rabbit Connected");
+							connected = true;
 						}
-					};
+						using (var channel = connection.CreateModel())
+						{
+							channel.QueueDeclare(cola, false, false, false, null);
 
-					channel.BasicConsume(queue: cola, autoAck: true, consumer: consumer);
+							var consumer = new EventingBasicConsumer(channel);
 
-					while (true) { }
+							consumer.Received += async (model, ea) =>
+							{
+								var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+
+								RepositoryCims repo = new RepositoryCims();
+
+								string idCim = message.Split(";")[1];
+
+								Cim cim = await repo.GetCim(idCim);
+
+								switch (message.Split(";")[0])
+								{
+									case "addRuta":
+										repo.addRuta(cim);
+										break;
+									case "deleteRuta":
+										repo.deleteRuta(cim);
+										break;
+									case "addRefugi":
+										repo.addRefugis(cim);
+										break;
+									case "deleteRefugi":
+										repo.deleteRefugis(cim);
+										break;
+								}
+							};
+
+							channel.BasicConsume(queue: cola, autoAck: true, consumer: consumer);
+
+							while (true) { }
+						}
+					}
+				}
+				catch (Exception)
+				{
+					Console.WriteLine("Rabbit Not connected");
+					System.Threading.Thread.Sleep(10000);
+
 				}
 			}
 		}
